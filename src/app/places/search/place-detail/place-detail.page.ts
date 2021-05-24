fb9163f01ed5738fdd34e4e91b6dd807a6301f0e
Lucas Venturella <lucas.venturella@getambush.com>
2021-05-24 13:35:49 -0300

    using action sheets

--- src/app/places/search/place-detail/place-detail.page.ts
+++ src/app/places/search/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
@@ -18,7 +18,8 @@ export class PlaceDetailPage implements OnInit {
     private route: ActivatedRoute,
     private navCtrl: NavController,
     private modalCtrl: ModalController,
-    private placesService: PlacesService) { }
+    private placesService: PlacesService,
+    private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
@@ -33,6 +34,29 @@ export class PlaceDetailPage implements OnInit {
   onBookPlace(){
     //this.router.navigateByUrl('/places/tabs/search');
     //this.navCtrl.navigateBack('/places/tabs/search');
+    this.actionSheetCtrl.create({
+      header: 'Choose an Action',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: () => {
+            this.openBookingModal('select');
+          }
+        },
+        {
+          text: 'Randon Date',
+          handler: () => {
+            this.openBookingModal('random');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: { selectedPlace: this.place }})
@@ -48,4 +72,20 @@ export class PlaceDetailPage implements OnInit {
       });
   }
 
+  openBookingModal(mode: 'select'|'random'){
+    console.log(mode);
+    this.modalCtrl.create({
+      component: CreateBookingComponent,
+      componentProps: { selectedPlace: this.place }})
+      .then(modalEl =>{
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then(resultData => {
+        console.log(resultData.data, resultData.role);
+        if(resultData.role === 'confirm'){
+          console.log('BOOKED!!!!');
+        }
+      });
+  }
 }
